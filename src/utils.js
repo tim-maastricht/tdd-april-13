57a56e52a68c4a207aeab3ee5efcb05940990973
Tim <tim.maastricht@gmail.com>
2026-04-13 20:21:06 +1000

    feat: add more complex tests

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,17 @@
-function sum(a, b) {
-    return a + b;
+// function sum(a, b) {
+//     return a + b;
+// }
+
+let tasks = [];
+
+function getTasks() {
+  // TODO
+  return tasks;
 }
 
-module.exports = { sum };
+function addTask(taskData) {
+  const task = { id: tasks.length + 1, ...taskData };
+  tasks.push(task);
+}
+
+module.exports = { getTasks, addTask };
--- tests/utils.test.js
+++ tests/utils.test.js
@@ -1,5 +1,23 @@
-const { sum } = require("../src/utils");
+// const { getTasks } = require("../src/utils");
 
-test('adds two numbers', () => {
-  expect(sum(2, 3)).toBe(5);
-})
+// test('adds two numbers', () => {
+//   expect(sum(2, 3)).toBe(5);
+// })
+
+// it('checks if the two added numbers are not 0', () => {
+//   expect(sum(2, 3)).not.toBe(0);
+// })
+
+const { getTasks, addTask } = require("../src/utils");
+
+it('should return empty array initially', () => {
+    expect(getTasks()).toEqual([]);
+});
+
+it('should add the first task', () => {
+    const taskData = { title: 'Buy milk' };
+    const result = addTask(taskData);
+
+    expect(result.title).toBe('Buy milk');
+    expect(result.id).toBe(1);
+});
