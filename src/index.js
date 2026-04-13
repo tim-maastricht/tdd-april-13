03db58d0df4238214fafe483b42ae417c5ee1cd8
Tim <tim.maastricht@gmail.com>
2026-04-13 20:41:37 +1000

    feat: add json tasks for index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const { getTasks, addTask } = require("./utils");
 
 const app = express();
 
 app.use(express.json());
+
+app.get('/tasks', (request, response) => {
+  response.json({
+    tasks: getTasks()
+  })
+})
+
+app.post('/tasks', (request, response) => {
+  const task = addTask(request.body);
+  response.status(201).json(task);
+})
 
 module.exports = app;
