4e49952a84e7d465794dfc2e1ab05926b29a0b7b
Tim <tim.maastricht@gmail.com>
2026-04-13 21:15:12 +1000

    linting index

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-const express = require("express");
-const { getTasks, addTask } = require("./utils");
+const express = require('express');
+const { getTasks, addTask } = require('./utils');
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // Parse JSON bodies
 
 app.get('/tasks', (request, response) => {
-  response.json({
-    tasks: getTasks()
-  })
-})
+    response.json({
+        tasks: getTasks()
+    });
+});
 
 app.post('/tasks', (request, response) => {
-  const task = addTask(request.body);
-  response.status(201).json(task);
-})
+    if (!request.body.title) {
+        return response.status(400).json({error: "Title is required."});
+    }
+
+    const task = addTask(request.body);
+    response.status(201).json(task);
+});
 
 module.exports = app;
